0b0ef71cb724310c5b6314a3f8249e44ba90e1e3
Aadhitya Sriram <aadhityasriram08@gmail.com>
2025-01-11 13:57:10 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from chat import App
+from langchain_core.messages import HumanMessage
+
+if 'app_instance' not in st.session_state:
+    st.session_state.app_instance = App(
+        system_prompt = """
+You are a medical assistant providing clear, accurate, and evidence-based healthcare responses in a natural and user-friendly manner.
+Instructions:
+- Focus on clarity, accuracy, and user safety.
+- Evaluate if the provided context is required first, and then use it to provide a response.
+- **Never** mention the context provided in your response.
+- If a query is not related to healthcare, politely inform the user and avoid answering unrelated topics.
+""",
+        template = """{
+            "Medical": {
+                "Disease": [],
+                "Symptom": [],
+                "Medication": [],
+                "Treatment": [],
+                "Procedure": [],
+                "Psychological factor": [],
+                "Allergen": [],
+                "Health Condition": [],
+                "Risk Factor": [],
+                "Dosage": [],
+                "Side Effect": [],
+                "Health Metric": [],
+                "Medical Terminology": []
+            }
+        }"""
+
+    )
+    st.session_state.messages = []
+    st.session_state.summary = ""
+
+app = st.session_state.app_instance
+
+st.set_page_config(
+    page_title="Medical Chatbot",
+    page_icon="💬",
+    layout="wide"
+)
+
+st.title("💬 Chat with Medical Assistant")
+
+for msg in st.session_state.messages:
+    if msg["role"] == "user":
+        with st.chat_message("user"):
+            st.markdown(msg["content"])
+    else:
+        with st.chat_message("assistant"):
+            st.markdown(msg["content"])
+
+user_query = st.chat_input("Ask a medical question...")
+
+if user_query:
+    with st.chat_message("user"):
+        st.markdown(user_query)
+    st.session_state.messages.append({"role": "user", "content": user_query})
+
+    with st.spinner("Thinking..."):
+        response = app.chat(user_query)
+    
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ chat.py
@@ -0,0 +1,148 @@
+import os
+import re
+import json
+import faiss
+import torch
+from uuid import uuid4
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.documents import Document
+from langchain_core.messages import HumanMessage, SystemMessage, RemoveMessage
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import MessagesState, StateGraph, START, END
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+
+class State(MessagesState):
+    summary: str
+
+class App:
+
+    def __init__(self, system_prompt, template):
+        load_dotenv()
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.ner_model_name = "numind/NuExtract-tiny-v1.5"
+        self.GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+        self.model = ChatGroq(model="llama3-8b-8192")
+        self.system_prompt = system_prompt
+        self.template = template
+        self.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        self.faiss_index = faiss.IndexFlatL2(768)
+        self.load_knowledge_base()
+        self.load_ner()
+        self.memory = MemorySaver()
+        self.workflow = StateGraph(State)
+        self.workflow.add_node("conversation", self.call_model)
+        self.workflow.add_node(self.summarize_conversation)
+        self.workflow.add_edge(START, "conversation")
+        self.workflow.add_conditional_edges("conversation", self.should_continue,)
+        self.workflow.add_edge("summarize_conversation", END)
+        self.app = self.workflow.compile(checkpointer=self.memory)
+        self.config = {"configurable": {"thread_id": "4"}}
+        print(f"Using device: {self.device}")
+        self.query_response(self.system_prompt)
+
+    def load_ner(self):
+        self.ner_model = AutoModelForCausalLM.from_pretrained(self.ner_model_name, torch_dtype=torch.bfloat16, trust_remote_code=True).to(self.device).eval()
+        self.ner_tokenizer = AutoTokenizer.from_pretrained(self.ner_model_name, trust_remote_code=True)
+
+    def load_knowledge_base(self):
+        if os.path.isdir("./knowledge_base"):
+            self.knowledge_base = FAISS.load_local(
+                "knowledge_base",
+                self.embeddings,
+                allow_dangerous_deserialization=True
+            )
+        else:
+            self.knowledge_base = FAISS(
+                embedding_function=self.embeddings,
+                index=self.faiss_index,
+                docstore=InMemoryDocstore(),
+                index_to_docstore_id={},
+            )
+            loader = PyPDFLoader(r'E:\PROJECTS\RAGMedBot\Documents\medical.pdf')
+            documents = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+            chunks = text_splitter.split_documents(documents)
+            uuids = [str(uuid4()) for _ in range(len(chunks))]
+            self.knowledge_base.add_documents(documents=chunks, ids=uuids)
+            self.knowledge_base.save_local("knowledge_base")
+
+    def load_past_context(self):
+        self.past_context = FAISS(
+            embedding_function=self.embeddings,
+            index=self.faiss_index,
+            docstore=InMemoryDocstore(),
+            index_to_docstore_id={},
+        )
+
+    def store_past_context(self, page_content):
+        document = Document(page_content=page_content)
+        self.past_context.add_documents([document])
+
+    def predict_ner(self, texts, batch_size=1, max_length=10_000, max_new_tokens=4_000):
+        template = json.dumps(json.loads(self.template), indent=4)
+        prompts = [f"""<|input|>\n### Template:\n{template}\n### Text:\n{text}\n\n<|output|>""" for text in texts]
+        outputs = []
+        with torch.no_grad():
+            for i in range(0, len(prompts), batch_size):
+                batch_prompts = prompts[i:i+batch_size]
+                batch_encodings = self.ner_tokenizer(batch_prompts, return_tensors="pt", truncation=True, padding=True, max_length=max_length).to(self.ner_model.device)
+                pred_ids = self.ner_model.generate(**batch_encodings, max_new_tokens=max_new_tokens, pad_token_id = self.ner_tokenizer.eos_token_id)
+                outputs += self.ner_tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+        prediction = [output.split("<|output|>")[1] for output in outputs][0]
+        return json.loads(prediction)
+
+    def is_medical_query(self, query):
+        entities = self.predict_ner([query])["Medical"]
+        medical_entities = [i for i in entities.items() if i[1]]
+        if (len(medical_entities) > 0):
+            return True
+        return False
+
+    def call_model(self, state):
+        summary = state.get("summary", "")
+        if summary:
+            system_message = f"Summary of conversation earlier: {summary}"
+            messages = [SystemMessage(content=system_message)] + state["messages"]
+        else:
+            messages = state["messages"]
+        response = self.model.invoke(messages)
+        return {"messages": [response]}
+
+    def should_continue(self, state):
+        messages = state["messages"]
+        if len(messages) > 2:
+            return "summarize_conversation"
+        return END
+
+    def summarize_conversation(self, state):
+        summary = state.get("summary", "")
+        if summary:
+            summary_message = (
+                f"This is summary of the conversation to date: {summary}\n\n"
+                "Extend the summary by taking into account the new messages above:"
+            )
+        else:
+            summary_message = "Create a summary of the conversation above:"
+
+        messages = state["messages"] + [HumanMessage(content=summary_message)]
+        response = self.model.invoke(messages)
+        delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+        return {"summary": response.content, "messages": delete_messages}
+
+    def query_response(self, input_msg):
+        input_message = HumanMessage(content=input_msg)
+        self.app.invoke({"messages": [input_message]}, self.config)
+
+    def chat(self, query):
+        if self.is_medical_query(query):
+            context = self.knowledge_base.similarity_search(query)[0].page_content
+            query = f"Context: {context}\n\n{query}"
+
+        self.query_response(query)
+        return self.app.get_state(self.config).values["messages"][-1].content
